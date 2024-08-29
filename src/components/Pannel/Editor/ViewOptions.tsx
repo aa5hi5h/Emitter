1f7d8584bd9b5d4a45702460ce594809e0984ddb
aashish <quakie.exe@gmail.com>
2024-08-29 18:28:21 +0530

    addedd the viewmode logic

--- src/components/Pannel/Editor/ViewOptions.tsx
+++ src/components/Pannel/Editor/ViewOptions.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
     Tooltip,
     TooltipContent,
@@ -5,26 +6,37 @@ import {
     TooltipTrigger,
   } from "@/components/ui/tooltip"
 import { Maximize, Monitor, Smartphone, Tablet, TabletSmartphone } from "lucide-react"
+import { useState } from "react"
   
+interface ViewOptionsProp{
+    ViewMode: (view:string) => void
+}
 
-const ViewOptions = () => {
+const ViewOptions : React.FC<ViewOptionsProp> = ({ViewMode}) => {
+    
+    const [selectedView,setSelectedView] = useState<string>()
+
+    const handleClick = (view:string) => {
+        setSelectedView(view)
+        ViewMode(view)
+    }
     return (
         <TooltipProvider>
         <Tooltip>
             <TooltipTrigger asChild>
-    <Monitor  className={`w-6 h-6 p-1 rounded-md hover:bg-slate-100 opacity-60 hover:opacity-100`} />
+    <Monitor onClick={() => handleClick("monitor")}  className={`w-6 h-6 p-1 rounded-md hover:bg-slate-100 opacity-60 hover:opacity-100`} />
     </TooltipTrigger>
     <TooltipContent>Dekstop</TooltipContent>
     </Tooltip>
     <Tooltip>
         <TooltipTrigger asChild>
-            <Tablet className={`w-6 h-6 p-1 rounded-md hover:bg-slate-100 opacity-60 hover:opacity-100`} />
+            <Tablet onClick={() => handleClick("tablet")} className={`w-6 h-6 p-1 rounded-md hover:bg-slate-100 opacity-60 hover:opacity-100`} />
         </TooltipTrigger>
         <TooltipContent>Tablet</TooltipContent>
     </Tooltip>
     <Tooltip>
         <TooltipTrigger>
-    <Smartphone  className={`w-6 h-6 p-1 rounded-md hover:bg-slate-100 opacity-60 hover:opacity-100 `} />
+    <Smartphone onClick={() => handleClick("mobile")} className={`w-6 h-6 p-1 rounded-md hover:bg-slate-100 opacity-60 hover:opacity-100 `} />
     </TooltipTrigger>
     <TooltipContent>Mobile</TooltipContent>
     </Tooltip>
@@ -32,7 +44,7 @@ const ViewOptions = () => {
     <hr className="border border-slate-400 h-6" />
     <Tooltip>
         <TooltipTrigger>
-    <Maximize  className={`w-6 h-6 p-1 rounded-md hover:bg-slate-100 opacity-60 hover:opacity-100`} />
+    <Maximize onClick={() => handleClick("maximize")}  className={`w-6 h-6 p-1 rounded-md hover:bg-slate-100 opacity-60 hover:opacity-100`} />
     </TooltipTrigger>
     <TooltipContent>Full Screen</TooltipContent>
     </Tooltip>
--- src/components/Pannel/Editor/index.tsx
+++ src/components/Pannel/Editor/index.tsx
@@ -5,9 +5,32 @@ import { ChevronDown, Redo, Undo } from "lucide-react"
 import {Frame, Element, useEditor} from "@craftjs/core"
 import { Container } from "../Components/Props/Container"
 import UndoOptions from "./UndoOptions"
+import { useState } from "react"
 const EditorPannel = () => {
 
+
+    const [viewMode,setViewMode] = useState<string>("monitor")
+
     const {actions,query} = useEditor()
+    
+
+    const viewScreen = () => {
+        switch (viewMode) {
+            case 'mobile':
+              return 'max-w-sm mx-auto';
+              case 'tablet':
+                return "max-w-md mx-auto"
+            case 'monitor':
+              return 'max-w-full';
+            default:
+              return '';
+          }
+    }
+
+    const handleViewMode = (view:string) => {
+        console.log(`View changed to: ${view}`);
+        setViewMode(view)
+    }
     return (
         <div className="flex flex-col h-full w-full">
             <div className="flex flex-row items-center py-2  border-b border-zinc-300 justify-between ">
@@ -22,15 +45,18 @@ const EditorPannel = () => {
                     <UndoOptions />
                     </span>
                 <span className="bg-slate-200 border-slate-300 border flex gap-2 items-center max-w-max p-2 rounded-md ml-auto mr-[2rem]">
-                    <ViewOptions />
+                    <ViewOptions ViewMode={handleViewMode}  />
                 </span>
                 </div>
             </div>
-            <Frame>
-                <Element is="div"  canvas  className="h-full bg-white m-1 p-1">
-                <p>Start building</p>
-            </Element>
-            </Frame>
+            
+            <div className={`h-full w-[99%] ${viewScreen()} bg-white border-[1px] border-slate-300 rounded-md m-1  p-1`}>
+        <Frame>
+          <Element is="div" canvas>
+            <p>Start building</p>
+          </Element>
+        </Frame>
+      </div>
         </div>
     )
 }
