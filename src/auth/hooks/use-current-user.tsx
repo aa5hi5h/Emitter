80ad68fa62e3026cccf463e11fb77432f06c2c9e
aashish <quakie.exe@gmail.com>
2024-09-17 05:57:04 +0530

    removed hooks

--- src/auth/hooks/use-current-user.tsx
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "convex/react"
-import { api } from "../../../convex/_generated/api"
-
-
-export const getCurrentUser = () => {
-    const data = useQuery(api.user.currentUser);
-    const isLoading = data === undefined;
-    return { data, isLoading };
-};
