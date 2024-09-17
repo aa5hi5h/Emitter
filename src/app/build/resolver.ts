f6798197168fe8ef540e840c73b009d742dbc4fd
aashish <quakie.exe@gmail.com>
2024-09-17 05:44:10 +0530

    separate resolver

--- src/app/build/resolver.ts
+++ src/app/build/resolver.ts
@@ -1,24 +1,19 @@
-"use client";
-
-import ComponentsPannel from "@/components/Pannel/Components";
-import EditorPannel from "@/components/Pannel/Editor";
-import PropertisePannel from "@/components/Pannel/Propertise";
-import { Editor } from "@craftjs/core";
-import { Text } from "../../../components/Pannel/Components/Props/Text";
+import { Editor, Resolver } from "@craftjs/core";
+import { Text } from "@/components/Pannel/Components/Props/Text";
 import { Container } from "@/components/Pannel/Components/Props/Container";
 import { CustomButton } from "@/components/Pannel/Components/Props/Button";
 import { CustomImage } from "@/components/Pannel/Components/Props/Image";
 import { CustomBanner } from "@/components/Pannel/Components/Props/Banner";
 import { CustomDiv } from "@/components/Pannel/Components/Props/Div";
-import { HoverProvider } from "../../Context/hoverContext";
-import { SelectionProvider } from "../../Context/selectionContext";
-import { ColorPickerProvider } from "../../Context/ColorPickerContext";
+import { HoverProvider } from "../Context/hoverContext";
+import { SelectionProvider } from "../Context/selectionContext";
+import { ColorPickerProvider } from "../Context/ColorPickerContext";
 import { useQuery } from "convex/react";
-import { api } from "../../../../convex/_generated/api";
-import { Id } from "../../../../convex/_generated/dataModel";
+import { api } from "../../../convex/_generated/api";
+import { Id } from "../../../convex/_generated/dataModel";
 import React from "react";
 import { EmptyCanvas } from "@/components/Pannel/Components/Props/EmptyCanvas";
-import { CanvasProvider, useCanvas } from '../../Context/CanvasContext';
+import { CanvasProvider, useCanvas } from '../Context/CanvasContext';
 import { CustomInput } from "@/components/Pannel/Components/Props/Input";
 import { CustomList } from "@/components/Pannel/Components/Props/List";
 import { CustomCard } from "@/components/Pannel/Components/Props/Card";
@@ -43,47 +38,44 @@ import { MotionTemplateDiv } from "@/components/Pannel/Components/Props/Themes/u
 import BlogDetailTemplate from "@/components/Pannel/Components/Props/Themes/BlogDetail";
 import { BedDouble, CableCar, Crown, Gem, HandPlatter, Hotel, Menu, Palmtree, Soup, Star, TentTree } from "lucide-react"
 import RentalLandingTemplate from "@/components/Pannel/Components/Props/Themes/RentalLanding";
-import { resolver } from "../resolver";
-
-interface BuildProjectProps{
-  params:{
-    buildID: Id<"projects">
-  }
-}
-
-const Build = ({params}: BuildProjectProps) =>  {
-
-  const { buildID } = params;
-  console.log("BuildID:", buildID)
 
 
-  const project = useQuery(api.project.getProject, { projectId:buildID });
-  const { checkIfCanvasEmpty } = useCanvas();
-
-  if(!project){
-    return null
-  }
-  return (
-    <SelectionProvider>
-      <HoverProvider>
-        <ColorPickerProvider>
-        <Editor resolver={resolver} onNodesChange={checkIfCanvasEmpty}>
-          <div className="grid grid-cols-6 h-[90vh] ">
-            <div className="col-span-1 border-r border-slate-300  h-full overflow-hidden">
-              <ComponentsPannel />
-            </div>
-            <div className="col-span-4 relative overflow-hidden border-r border-slate-300 h-full">
-              <EditorPannel buildID={project._id} project={project} savedState={project.savedState} />
-            </div>
-            <div className="col-span-1 h-full overflow-hidden" >
-              <PropertisePannel />
-            </div>
-          </div>
-        </Editor>
-        </ColorPickerProvider>
-      </HoverProvider>
-    </SelectionProvider>
-  );
-}
-
-export default Build
+export const resolver: Resolver = {
+    Text,
+    Container,
+    CustomButton,
+    CustomImage,
+    CustomBanner,
+    CustomInput,
+    CustomDiv,
+    CustomList,
+    CustomCard,
+    CustomNavbar,
+    CustomVideo,
+    CustomCarousel,
+    EmptyCanvas,
+    AccordionFAQ,
+    PricingSection,
+    AnimatedGradientBackground,
+    TestimonialCarousel,
+    LandingPage,
+    MultiStepForm,
+    HeroSection,
+    ImageBackground,
+    FeaturesSection,
+    FeatureCard,
+    TemplateText,
+    TemplateDiv,
+    EcommerceNewLandingTemplate,
+    ChevronRight,Image,Box,ArrowUpRight,
+    BedDouble, CableCar, Crown,
+    Gem, HandPlatter, Hotel,CircleUser,
+     Menu, Palmtree, Soup, Star, TentTree ,
+    TemplateImage,
+    LmsLandingTemplate,
+    BlogLandingTemplate,
+    MotionTemplateDiv,
+    StoreCollectionTemplate,
+    RentalLandingTemplate,
+    BlogDetailTemplate
+  };
